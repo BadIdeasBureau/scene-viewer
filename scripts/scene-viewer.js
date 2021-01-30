7e3189529db6dd8cfa6aee5f140bc15de00e3815
BadIdeasBureau <dumbmuscle@gmail.com>
2021-01-30 22:35:45 +0000

    Integrating MultiMedia Popout

--- scripts/scene-viewer.js
+++ scripts/scene-viewer.js
@@ -8,9 +8,6 @@ Hooks.on("renderCompendium", (compendium, html, data) => {
         const scene = await fromUuid(`Compendium.${data.collection}.${target.dataset.entryId}`);
         const image = scene.data.img;
         let dialogType= "Loading";
-        if (image.includes(".webm")){
-            dialogType = "Webm"
-        }
         let loading = new Dialog({
             title: game.i18n.localize(`SCENE_VIEWER.${dialogType}.Title`),
             content: game.i18n.localize(`SCENE_VIEWER.${dialogType}.Content`),
@@ -24,7 +21,7 @@ Hooks.on("renderCompendium", (compendium, html, data) => {
            });
         loading.render(true);
         if (dialogType === "Loading"){
-            new ImagePopout(image).render(true);
+            new MultiMediaPopout(image).render(true);
             Hooks.once("renderImagePopout",()=>{
                 loading.close()
             })
@@ -33,5 +30,66 @@ Hooks.on("renderCompendium", (compendium, html, data) => {
 });
 
 //also todo - add an option into the context menu?
-//todo: Implement MultiMediaPopout from Zeel's library
 //Stretch goals:  Settings to change modifier key (ctrl/shift/alt)
+
+/**
+ * Capable of handling images, as well as .mp4 and .webm video
+ * not very sophisticated (according to Zeel). Original code by Zeel, used under MIT license
+ *
+ * @class MultiMediaPopout
+ * @extends {ImagePopout}
+ */
+class MultiMediaPopout extends ImagePopout {
+	/**
+	 * Creates an instance of MultiMediaPopout.
+	 *
+	 * @param {string} src
+	 * @param {object} [options={}]
+	 * @memberof MultiMediaPopout
+	 */
+	constructor(src, options = {}) {
+		super(src, options);
+
+		this.video = ["mp4", "webm"].includes(
+			src.filename.split('.').pop().toLowerCase()
+		);
+
+		this.options.template = "modules/scene-viewer/templates/media-popout.html"; //TODO: make the "muted" part of the template optional
+	}
+
+	/** @override */
+	async getData(options) {
+		let data = await super.getData();
+		data.isVideo = this.video;
+		return data;
+	}
+	/**
+	* Share the displayed image with other connected Users
+	*/
+	shareImage() {
+		game.socket.emit("module.token-hud-art-button", {
+			image: this.object,
+			title: this.options.title,
+			uuid: this.options.uuid
+		});
+	}
+
+	/**
+	 * Handle a received request to display media.
+	 *
+	 * @override
+	 * @param {string} image - The path to the image/media resource.
+	 * @param {string} title - The title for the popout title bar.
+	 * @param {string} uuid
+	 * @return {MultiMediaPopout}
+	 * @private
+	 */
+	static _handleShareMedia({ image, title, uuid } = {}) {
+		return new MultiMediaPopout(image, {
+			title: title,
+			uuid: uuid,
+			shareable: false,
+			editable: false
+		}).render(true);
+	}
+}
